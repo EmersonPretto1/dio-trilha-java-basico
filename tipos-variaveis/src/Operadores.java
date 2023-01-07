7d75a863cb2cd92d874f455cbb8166c6989cd332
EmersonPretto1 <86754482+EmersonPretto1@users.noreply.github.com>
2023-01-07 11:22:48 -0300

    incremento de soma

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,13 +2,14 @@ public class Operadores {
     public static void main(String[] args) {
         int numero = 5;
 
-        numero = -numero;
-
+        numero = numero + 1;
         System.out.println(numero);
+
+        System.out.println(numero++);
         
-        numero = numero * -1;
+        
 
-        System.out.println(numero);
+
     }
     
 }
