ea341a2b191cf3cfef65d29f6e7cd3c6e720023f
EmersonPretto1 <86754482+EmersonPretto1@users.noreply.github.com>
2023-01-07 11:30:13 -0300

    inversao de valor logico

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -5,7 +5,14 @@ public class Operadores {
         numero = numero + 1;
         System.out.println(numero);
 
-        System.out.println(numero++);
+        numero = 5;
+
+        System.out.println(numero--);
+        System.out.println(numero--);
+
+        boolean variavel = true;
+        System.out.println(variavel);
+        System.out.println(!variavel);
         
         
 
