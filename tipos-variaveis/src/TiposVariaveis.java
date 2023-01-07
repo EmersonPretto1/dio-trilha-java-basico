228dab1c71556059da3a80c619915f5e89ea64cc
EmersonPretto1 <86754482+EmersonPretto1@users.noreply.github.com>
2023-01-07 12:05:42 -0300

    atualizacao

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,6 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2.500;
+        
 
         final double VALORDEPI = 3.14;
         System.out.println(VALORDEPI );
