18c972514710922ffccf2caafd67fc792dc57b3b
EmersonPretto1 <86754482+EmersonPretto1@users.noreply.github.com>
2023-01-07 12:01:42 -0300

    operadores relacionais

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,27 +1,15 @@
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
-
-        numero = numero + 1;
-        System.out.println(numero);
-
-        numero = 5;
-
-        System.out.println(numero--);
-        System.out.println(numero--);
-
-        boolean variavel = true;
-        System.out.println(variavel);
-        System.out.println(!variavel);
-
-        int a, b;
-
-        a = 5;
-        b = 6;
-
-        String resultado = a==b ? "verdadeiro" : "falso";
+        boolean condicao1 = true;
         
-        System.out.println(resultado);    
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("as duas condicoes são verdadeiras");
+
+        }
+        
+        System.out.println("fim");
             
         
         
